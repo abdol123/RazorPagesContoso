5813c626bcb9f66e7c815bca3e6d9db44f51369d
21706644 <21706644@HSPD15083493>
2019-11-28 09:29:55 +0000

    Add project files.

---
+++ AH_RazorPagesContoso/Pages/Hello.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AH_RazorPagesContoso.Pages
+{
+    public class HelloModel : PageModel
+    {
+        public string UserName { get; set; }
+       
+        public void OnGet()
+        {
+            UserName = null;
+        }
+
+        public void OnPost()
+        {
+            UserName = Request.Form["UserName"];
+        }
+    }
+}
